88cb6d71fba6d28917fbf9860ac5ecadb41e84c8
hadi892 <amerianh3@gmail.com>
2026-08-18 20:57:09 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,27 @@
+plugins {
+    id("com.android.application") version "9.1.1"
+}
 
+android {
+    namespace = "com.virtualdishdsp"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.virtualdishdsp"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+}
