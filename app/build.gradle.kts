6c9722619d50b04424611502ee50f6c387342cbd
hadi892 <amerianh3@gmail.com>
2026-08-18 20:33:05 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,31 @@
+plugins {
+    id("com.android.application") version "9.0.0"
+}
 
+android {
+    namespace = "com.virtualdishdsp"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.virtualdishdsp"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+}
